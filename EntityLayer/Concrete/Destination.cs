7149f947e53384c46d021e6a9896376099133178
YakupAlmatepe <almatepe.yakup@gmail.com>
2023-08-22 12:50:23 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Destination.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    // entityLayer katmanında her bir class bir VERİ TABANI TABLOSUDUR
+    // PROPLAR İSE BU TABLONUN COLUMN U TEMSİL EDER BU YÜZDEN dESTİNATİON ADINDA BİR TABLOMUZ VAR VE İÇİNCDE BU TABLONUNPROPLARI YANİ DİĞER VERİLERİ MEVCUTTUR
+    public class Destination
+    {
+        [Key]
+        public int DestinationID { get; set; }
+        public string City { get; set; }
+        public string DayNight{ get; set; }
+        public double Price{ get; set; }
+        public string Image{ get; set; }
+        public string Description{ get; set; }
+        public int Capacity{ get; set; }
+        public bool Status{ get; set; }
+    }
+}
